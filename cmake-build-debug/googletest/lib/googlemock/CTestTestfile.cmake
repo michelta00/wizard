22e9389c8134ee15e96b3a4bb3b3eaa2d79a980a
marie3003 <marie.viktoria.becker@gmail.com>
2024-10-28 15:32:13 +0100

    added example project

---
+++ cmake-build-debug/googletest/lib/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/mariebecker/Documents/Uni/ETH/3_Semester/SoftwareEngineering/wizard/googletest/lib/googlemock
+# Build directory: /Users/mariebecker/Documents/Uni/ETH/3_Semester/SoftwareEngineering/wizard/cmake-build-debug/googletest/lib/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
---
+++ cmake-build-debug/googletest/lib/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/mariebecker/Documents/Uni/ETH/3_Semester/SoftwareEngineering/wizard/googletest/lib/googletest
+# Build directory: /Users/mariebecker/Documents/Uni/ETH/3_Semester/SoftwareEngineering/wizard/cmake-build-debug/googletest/lib/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
