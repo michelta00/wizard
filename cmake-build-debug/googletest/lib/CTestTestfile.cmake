f2353a7a7534edb14dd3bfc95e67283da6a3b6c5
marie3003 <marie.viktoria.becker@gmail.com>
2024-10-31 11:45:31 +0100

    Update .gitignore and remove tracked build files

--- cmake-build-debug/googletest/lib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/mariebecker/Documents/Uni/ETH/3_Semester/SoftwareEngineering/wizard/googletest/lib
-# Build directory: /Users/mariebecker/Documents/Uni/ETH/3_Semester/SoftwareEngineering/wizard/cmake-build-debug/googletest/lib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
