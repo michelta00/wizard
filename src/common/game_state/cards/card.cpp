cad1da2a6129373bb02dde1add121dc47252bdaf
mtarnow <mtarnow@ethz.ch>
2024-11-08 14:22:06 +0100

    updated card and hand classes from lama to wizard

--- src/common/game_state/cards/card.cpp
+++ src/common/game_state/cards/card.cpp
@@ -1,40 +1,62 @@
-//
-// Created by Manuel on 25.01.2021.
-//
-
 #include "card.h"
-
+#include "trick.h"
+#include "../player/hand.h"
 #include "../../exceptions/WizardException.h"
 
-
+// constructors
 card::card(std::string id) : unique_serializable(id) { }
 
-card::card(std::string id, serializable_value<int> *val)
+card::card(std::string id, serializable_value<std::tuple<int, int>> *val)
         : unique_serializable(id), _value(val)
 { }
 
-card::card(int val) :
+card::card(std::tuple<int, int> val) :
         unique_serializable(),
-        _value(new serializable_value<int>(val))
+        _value(new serializable_value<std::tuple<int, int>>(val))
 { }
 
-card::~card() { }
+// destructor
+card::~card() = default;
 
-
-int card::get_value() const noexcept {
+// getter function, returns tuple of two ints indicating the number of the card and its color
+std::tuple<int, int> card::get_value() const noexcept {
     return _value->get_value();
 }
 
-bool card::can_be_played_on(const card *const other) const noexcept {
-    // return true if this card has a one higher or of equal value OR if 'other' is Wizard and this is 1
-    int value_delta = this->get_value() - other->get_value();
-    return value_delta == 0 || value_delta == 1 || (other->get_value() == 7 && this->get_value() == 1);
+bool card::can_be_played(const trick* const current_trick, const hand* const current_hand) const noexcept {
+    // return true if this card can be played according to the game rules:
+    // if the card is a jester or wizard, it can be played
+    // it the card has the same color as the trick color, it can be played
+    // if the card does not have the same color as the trick color, it can only be played if no other card
+    // on the players hand has the same color as the trick color
+
+    // get values of this card
+    std::tuple<int, int> value = this->get_value();
+
+    // check if card is wizard or jester
+    if (std::get<0>(value) > 13) return true;
+
+    // if card is not wizard or jester, compare with trick color
+    int trick_color = current_trick->get_trick_color();
+    if (std::get<1>(value) == trick_color) return true;
+
+    // TODO: make more efficient if possible
+    // if card does not have trick color, check other cards on hand
+    const std::vector<card*> cards = current_hand->get_cards();
+    for (auto it = cards.begin(); it != cards.end(); ++it)
+    {
+        std::tuple<int, int> current_value = (*it)->get_value();
+        // if there is a card on the hand with the trick color, the current card cannot be played
+        if (std::get<1>(current_value) == trick_color) return false;
+    }
+
+    return true;
 }
 
 
 card *card::from_json(const rapidjson::Value &json) {
     if (json.HasMember("id") && json.HasMember("value")) {
-        return new card(json["id"].GetString(), serializable_value<int>::from_json(json["value"].GetObject()));
+        return new card(json["id"].GetString(), serializable_value<std::tuple<int, int>>::from_json(json["value"].GetObject()));
     } else {
         throw WizardException("Could not parse json of card. Was missing 'id' or 'val'.");
     }
