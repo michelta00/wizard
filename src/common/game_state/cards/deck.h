a28560e57e6128c8d001409ea3bf9bfddc2b8e48
mtarnow <mtarnow@ethz.ch>
2024-11-21 12:29:53 +0100

    updated deck class and deleted discard_pile and draw_pile classes

--- src/common/game_state/cards/deck.h
+++ src/common/game_state/cards/deck.h
@@ -21,35 +21,35 @@ class deck : public unique_serializable
 private:
     std::vector<card*> _all_cards;
     std::vector<card*> _remaining_cards;
-    card* _last_card = nullptr;
 
     // deserialization constructor
-    deck(std::string id, std::vector<card*> &all_cards, std::vector<card*> &remaining_cards, card* last_card);
+    deck(const std::string& id, const std::vector<card*> &all_cards, const std::vector<card*> &remaining_cards);
 
     // from_diff constructor
-    explicit deck(std::string id);
+    explicit deck(const std::string& id);
 
 public:
     // constructors & destructor
     deck();     // main constructor which should be used by game state to create its deck
-    explicit deck(std::vector<card*>& cards);
+    explicit deck(const std::vector<card*>& cards);
     ~deck();
 
     // accessors
-    bool is_empty() const noexcept;
-    int get_number_of_remaining_cards() const noexcept;
+    [[nodiscard]] bool is_empty() const noexcept;
+    [[nodiscard]] unsigned int get_number_of_remaining_cards() const noexcept;
 
 #ifdef WIZARD_SERVER
     // state update functions
-    void setup_round(std::string& err);                                     // sets up the new round essentially by
-                                                                            // refreshing _remaining_cards by copying
-                                                                            // from _all_cards, and resets _last_card
-    bool draw_cards(player* player, int round_number, std::string& err);    // draws a number of random cards based on
-                                                                            // the current round number and places them
-                                                                            // into the player's hand
-    card* draw_trump() const;                                               // randomly draws a card from the remaining
-                                                                            // cards as a trump, or if all cards have
-                                                                            // been dealt, uses the _last_card as trump
+    void setup_round();     // sets up the new round essentially by
+                            // refreshing _remaining_cards by copying
+                            // from _all_cards, and resets _last_card
+    bool draw_cards(const player* player, int round_number, std::string& err);      // draws a number of random cards
+                                                                                    // based
+                                                                                    // on the current round number and
+                                                                                    // places them into the player's hand
+    [[nodiscard]] card* draw_trump() const;       // randomly draws a card from the remaining
+                                    // cards as a trump, or if all cards have
+                                    // been dealt, uses the _last_card as trump
 #endif
 
     // serialization
