6bbc6d4aedcfabbeb90716901a475703575ab5aa
mtarnow <mtarnow@ethz.ch>
2024-12-14 12:56:19 +0100

    correct prepare ubuntu

--- scripts/prepare_ubuntu.sh
+++ scripts/prepare_ubuntu.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install build-essential
-sudo apt-get install libwxgtk3.0-gtk3-dev
+sudo apt-get install libwxgtk3.2-dev
 sudo apt-get install cmake
+sudo reboot
